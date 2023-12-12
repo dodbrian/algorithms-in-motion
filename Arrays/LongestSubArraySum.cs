edc4f7e33da36268aa88eddf36d4ad67b7dbacf3
Denis Zimin <roydronov@gmail.com>
2023-12-12 20:54:27 +0100

    add Longest Sub Array Sum

---
+++ Arrays/LongestSubArraySum.cs
@@ -0,0 +1,25 @@
+// Given an array of positive integers nums and an integer k,
+// find the length of the longest subarray whose sum is less than or equal to k.
+
+namespace Arrays;
+
+public static class LongestSubArraySum
+{
+    public static int Calculate(int[] nums, int k)
+    {
+        var left = 0;
+        var currentSum = 0;
+        var longest = 0;
+
+        for (var right = 0; right < nums.Length; right++)
+        {
+            currentSum += nums[right];
+            while (currentSum > k && left < right) currentSum -= nums[left++];
+
+            var length = right - left + 1;
+            longest = longest < length ? length : longest;
+        }
+
+        return longest;
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using Arrays;
+
+Console.WriteLine("Test Sorted and Combined");
+
+var arr1 = new[] { 1, 3, 7, 11, 15, 23, 116 };
+var arr2 = new[] { 1, 4, 5, 18, 25, 43, 116, 124, 221 };
+
+var result = SortedAndCombined.Combine2(arr1, arr2);
+
+foreach (var i in result)
+{
+    Console.Write($"{i} ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Test Longest Sub Array Sum");
+
+var longest = LongestSubArraySum.Calculate(arr2, 80);
+
+Console.WriteLine(longest);
