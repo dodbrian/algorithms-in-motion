e52c338512a7d562dceb943176ef9df6019fd7ac
Denis Zimin <roydronov@gmail.com>
2023-12-21 00:54:25 +0100

    check sub array sums within limit

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -37,3 +37,14 @@ var fixedWindowArray = new[] { 3, -1, 4, 12, -8, 5, 6 };
 var subArraySum = LargestFixedSubArraySum.Calculate(fixedWindowArray, 4);
 
 Console.WriteLine(subArraySum);
+
+Console.WriteLine("Sub Array Query Limit");
+
+var arrayForSum = new[] { 2, 8, 16, 32, 4, 1, 23, 11, 9, 28 };
+var queries = new[] { new[] { 1, 8 }, new[] { 2, 3 }, new[] { 0, 9 }, new[] { 5, 7 } };
+const int limit = 123;
+
+var limitResults = CheckSubarraySumsWithinLimit.Calculate(arrayForSum, queries, limit);
+
+foreach (var (query, isWithinLimit) in queries.Zip(limitResults))
+    Console.WriteLine($"{query[0]}, {query[1]}, {isWithinLimit}");
